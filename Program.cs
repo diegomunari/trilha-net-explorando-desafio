56aa05b30330ea4ce66fcecadc528e5daff38262
Diego Munari <diegomunari@gmail.com>
2024-08-18 09:46:49 -0300

    Finalizado o código de exemplo

--- Program.cs
+++ Program.cs
@@ -16,10 +16,17 @@ hospedes.Add(p2);
 Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 10);
 reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
+try
+{
+    reserva.CadastrarHospedes(hospedes);
+    // Exibe a quantidade de hóspedes e o valor da diária
+    Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+    Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+}
+catch (System.Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
 
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
